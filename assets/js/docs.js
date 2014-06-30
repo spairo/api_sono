ba554bbe01ea66989ad8a39aecadb2f5a60176c8
spairo <spairo.psychodelic@gmail.com>
2014-06-30 01:26:57 -0500

    Angular JS Implemented
    Angular JS :)

--- assets/js/docs.js
+++
@@ -1,7 +0,0 @@
-/**
- * Documentation JS.
- * 
- */
-$(function(){
-    prettyPrint();
-});
--- js/app.js
+++ js/app.js
@@ -1,16 +1,45 @@
-// Filename: app.js
-define([
-  'jquery',
-  'underscore',
-  'backbone',
-  'router', // Request router.js
-], function($, _, Backbone, Router){
-  var initialize = function(){
-    // Pass in our Router module and call it's initialize function
-    Router.initialize();
-  };
+// Router UI
 
-  return {
-    initialize: initialize
-  };
+var routerApp = angular.module('routerApp', ['ui.router', 'ngAnimate', 'audiometa']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        //Default Module
+
+        .state('home', {
+            url: '/home',
+            templateUrl: 'templates/tmp_home.html'
+        })
+
+        //Upload Module
+
+        .state('upload', {
+            url: '/upload',
+            templateUrl: 'templates/upload/uploadTemplate.html'
+        })
+            .state('music', {
+                url: '/upload/music',
+                templateUrl: 'templates/upload/musicTemplate.html'
+            })
+        //Accounts Module
+
+        .state('accounts', {
+            url: '/accounts',
+            templateUrl: 'templates/accounts/accountsTemplate.html'
+        })
+            .state('create', {
+                url: '/accounts/create',
+                templateUrl: 'templates/accounts/createTemplate.html'
+            })
+
+
+        .state('about', {
+            url: '/about',
+            templateUrl: 'templates/tmp_about.html'
+        });
+
 });
