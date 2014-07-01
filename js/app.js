b539bd056bcdd1b52ea8b940fc35175b5172829f
spairo <spairo.psychodelic@gmail.com>
2014-07-01 03:03:00 -0500

    UploadController added
    I’ve added UploadController and source :)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-// Router UI
+// appSonofe CMS v.1
 
 var appSonofe = angular.module('appSonofe', ['ui.router', 'ngAnimate', 'angularFileUpload']);
 
-appSonofe.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
 
-    $stateProvider
+  appSonofe.config(function($stateProvider, $urlRouterProvider){
+
+      $urlRouterProvider.otherwise('/home');
+
+      $stateProvider
 
         //Default Module
 
@@ -42,90 +44,97 @@ appSonofe.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: 'templates/tmp_about.html'
         });
 
-});
+  });
 
+  //Upload Manager files
 
-//Basic upload
+  appSonofe.controller('UploadController', function ($scope, $fileUploader) {
+      'use strict';
 
-appSonofe.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
-}]);
-
-appSonofe.service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function(file, uploadUrl){
-        var fd = new FormData();
-        fd.append('conference_file', file);
-        $http.post(uploadUrl, fd, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        })
-        .success(function(data, status, headers, config){
-          alert("file is uploaded successfully.."+data+"..status.."+status);
-          // file is uploaded successfully
-          console.log(data);
-        })
-        .error(function(data, status, headers, config){
-          alert("algo fallo."+data+"..status.."+status);
-          // file is uploaded successfully
-          console.log(data);
-        });
-    }
-}]);
-
-appSonofe.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
-
-    $scope.uploadFile = function(){
-        var file = $scope.myFile;
-        console.log('file is ' + JSON.stringify(file));
-        var uploadUrl = "http://godster.mx:5000/conference";
-        fileUpload.uploadFileToUrl(file, uploadUrl);
-    };
-
-}]);
-
-//Upload Manager files
-
-var UploadCtrl = [ '$scope', '$upload', function($scope, $upload) {
-  $scope.onFileSelect = function($files) {
-    //$files: an array of files selected, each file has name, size, and type.
-    for (var i = 0; i < $files.length; i++) {
-      var file = $files[i];
-      $scope.upload = $upload.upload({
-        url: 'http://godster.mx:5000/conference', //upload.php script, node.js route, or servlet url
-        // method: 'POST' or 'PUT',
-        // headers: {'header-key': 'header-value'},
-        // withCredentials: true,
-        data: {myObj: $scope.myModelObj},
-        file: file, // or list of files: $files for html5 only
-        /* set the file formData name ('Content-Desposition'). Default is 'file' */
-        //fileFormDataName: myFile, //or a list of names for multiple files (html5).
-        /* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
-        //formDataAppender: function(formData, key, val){}
-      }).progress(function(evt) {
-        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-      }).success(function(data, status, headers, config) {
-        // file is uploaded successfully
-        console.log(data);
+      // create a uploader with options
+      var uploader = $scope.uploader = $fileUploader.create({
+          scope: $scope,                          // to automatically update the html. Default: $rootScope
+          url: 'upload.php',
+          formData: [
+              { key: 'value' }
+          ],
+          filters: [
+              function (item) {                    // first user filter
+                  console.info('filter1');
+                  return true;
+              }
+          ]
       });
-      //.error(...)
-      //.then(success, error, progress);
-      //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
-    }
-    /* alternative way of uploading, send the file binary with the file's content-type.
-       Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed.
-       It could also be used to monitor the progress of a normal http post/put request with large data*/
-    // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
-  };
-}];
+
+
+      // FAQ #1
+      var item = {
+          file: {
+              name: 'Previously uploaded file',
+              size: 1e6
+          },
+          progress: 100,
+          isUploaded: true,
+          isSuccess: true
+      };
+      item.remove = function() {
+          uploader.removeFromQueue(this);
+      };
+      uploader.queue.push(item);
+      uploader.progress = 100;
+
+
+      // ADDING FILTERS
+
+      uploader.filters.push(function (item) { // second user filter
+          console.info('filter2');
+          return true;
+      });
+
+      // REGISTER HANDLERS
+
+      uploader.bind('afteraddingfile', function (event, item) {
+          console.info('After adding a file', item);
+      });
+
+      uploader.bind('whenaddingfilefailed', function (event, item) {
+          console.info('When adding a file failed', item);
+      });
+
+      uploader.bind('afteraddingall', function (event, items) {
+          console.info('After adding all files', items);
+      });
+
+      uploader.bind('beforeupload', function (event, item) {
+          console.info('Before upload', item);
+      });
+
+      uploader.bind('progress', function (event, item, progress) {
+          console.info('Progress: ' + progress, item);
+      });
+
+      uploader.bind('success', function (event, xhr, item, response) {
+          console.info('Success', xhr, item, response);
+      });
+
+      uploader.bind('cancel', function (event, xhr, item) {
+          console.info('Cancel', xhr, item);
+      });
+
+      uploader.bind('error', function (event, xhr, item, response) {
+          console.info('Error', xhr, item, response);
+      });
+
+      uploader.bind('complete', function (event, xhr, item, response) {
+          console.info('Complete', xhr, item, response);
+      });
+
+      uploader.bind('progressall', function (event, progress) {
+          console.info('Total progress: ' + progress);
+      });
+
+      uploader.bind('completeall', function (event, items) {
+          console.info('Complete all', items);
+      });
+
+  });
