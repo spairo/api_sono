d66bea170ef5d4cd52ed329196202b80ab89b21a
spairo <spairo.psychodelic@gmail.com>
2014-06-30 23:42:02 -0500

    upload method changed
    upload method fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Router UI
 
-var appSonofe = angular.module('appSonofe', ['ui.router', 'ngAnimate']);
+var appSonofe = angular.module('appSonofe', ['ui.router', 'ngAnimate', 'angularFileUpload']);
 
 appSonofe.config(function($stateProvider, $urlRouterProvider) {
 
@@ -44,6 +44,8 @@ appSonofe.config(function($stateProvider, $urlRouterProvider) {
 
 });
 
+
+//Basic upload
 
 appSonofe.directive('fileModel', ['$parse', function ($parse) {
     return {
@@ -92,3 +94,38 @@ appSonofe.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpl
     };
 
 }]);
+
+//Upload Manager files
+
+var UploadCtrl = [ '$scope', '$upload', function($scope, $upload) {
+  $scope.onFileSelect = function($files) {
+    //$files: an array of files selected, each file has name, size, and type.
+    for (var i = 0; i < $files.length; i++) {
+      var file = $files[i];
+      $scope.upload = $upload.upload({
+        url: 'http://godster.mx:5000/conference', //upload.php script, node.js route, or servlet url
+        // method: 'POST' or 'PUT',
+        // headers: {'header-key': 'header-value'},
+        // withCredentials: true,
+        data: {myObj: $scope.myModelObj},
+        file: file, // or list of files: $files for html5 only
+        /* set the file formData name ('Content-Desposition'). Default is 'file' */
+        //fileFormDataName: myFile, //or a list of names for multiple files (html5).
+        /* customize how data is added to formData. See #40#issuecomment-28612000 for sample code */
+        //formDataAppender: function(formData, key, val){}
+      }).progress(function(evt) {
+        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+      }).success(function(data, status, headers, config) {
+        // file is uploaded successfully
+        console.log(data);
+      });
+      //.error(...)
+      //.then(success, error, progress);
+      //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
+    }
+    /* alternative way of uploading, send the file binary with the file's content-type.
+       Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed.
+       It could also be used to monitor the progress of a normal http post/put request with large data*/
+    // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
+  };
+}];
