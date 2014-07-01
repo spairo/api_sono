d46e586b8dd155a0ba5e84be76f708b069360d3a
spairo <spairo.psychodelic@gmail.com>
2014-07-01 04:07:19 -0500

    foo test
    testing

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ var appSonofe = angular.module('appSonofe', ['ui.router', 'ngAnimate', 'angularF
       // create a uploader with options
       var uploader = $scope.uploader = $fileUploader.create({
           scope: $scope,                          // to automatically update the html. Default: $rootScope
-          url: '/conference',
+          url: 'http://godster.mx/conference',
           method: 'POST',
           alias: 'conference_file',
           isHTML5: true,
@@ -142,3 +142,49 @@ var appSonofe = angular.module('appSonofe', ['ui.router', 'ngAnimate', 'angularF
       });
 
   });
+
+
+  //test mode
+
+  appSonofe.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+  }]);
+
+  appSonofe.service('fileUpload', ['$http', function ($http) {
+      this.uploadFileToUrl = function(file, uploadUrl){
+          var fd = new FormData();
+          fd.append('conference_file', file);
+          $http.post(uploadUrl, fd, {
+              transformRequest: angular.identity,
+              headers: {'Content-Type': undefined}
+          })
+          .success(function(response){
+            alert("well done");
+          })
+          .error(function(){
+          });
+      }
+  }]);
+
+  appSonofe.controller('fooCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
+
+      $scope.uploadFile = function(){
+          var file = $scope.myFile;
+          console.log('file is ' + JSON.stringify(file));
+          var uploadUrl = "http://godster.mx/conference";
+          fileUpload.uploadFileToUrl(file, uploadUrl);
+      };
+
+  }]);
