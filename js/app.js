f13648736fc6eaf02cc4348588d0ae7f86db581b
spairo <spairo.psychodelic@gmail.com>
2014-07-23 13:59:38 -0500

    uploader.files 1 vertion
    I’ve changed to first version.

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ var appSonofe = angular.module('appSonofe', ['ui.router', 'ngAnimate', 'angularF
 
         .state('about', {
             url: '/about',
-            templateUrl: 'js/views/tmp_about.html'
+            templateUrl: 'js/views/pages/about.html'
         });
 
   });
@@ -120,57 +120,6 @@ var appSonofe = angular.module('appSonofe', ['ui.router', 'ngAnimate', 'angularF
       };
 
   });
-
-  // Uploader Files
-
-  var filesCtrl = [ '$scope', '$upload', function($scope, $upload) {
-
-    $scope.onFileSelect = function($files) {
-      //$files: an array of files selected, each file has name, size, and type.
-      for (var i = 0; i < $files.length; i++) {
-
-        var file = $files[i];
-
-        $scope.upload = $upload.upload({
-
-            url: 'http://godster.mx/conference', //servlet url
-            method: 'POST',
-            //headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            //withCredentials: true,
-
-            data: { conference_file: $scope.myModelObj},
-
-            file: file, // or list of files ($files) for html5 only
-
-            //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-
-            // customize file formData name ('Content-Desposition'), server side file variable name.
-
-            //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file'
-
-            // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
-
-            //formDataAppender: function(formData, key, val){}
-
-        }).progress(function(evt) {
-
-          console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-
-        }).success(function(data, status, headers, config) {
-          // file is uploaded successfully
-          console.log(data);
-
-        });
-        //.error(...)
-        //.then(success, error, progress);
-        // access or attach event listeners to the underlying XMLHttpRequest.
-        //.xhr(function(xhr){xhr.upload.addEventListener(...)})
-      }
-    };
-
-  }];
-
-
 
   //Upload Manager files
 
